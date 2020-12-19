ca151284f23e42e38ff663b98affb58d9a60b3da
Seniorendi <info@srendi.de>
2020-12-19 19:29:05 +0100

    Changed config messages

--- src/main/java/de/srendi/intelligencetweaks/common/configuration/IntelligenceConfig.java
+++ src/main/java/de/srendi/intelligencetweaks/common/configuration/IntelligenceConfig.java
@@ -25,12 +25,12 @@ public class IntelligenceConfig {
 
         COMMON_BUILDER.comment("").push("Join Stuff");
 
-        SEND_JOIN_MESSAGE = COMMON_BUILDER.comment("Should the mod send the Join Message").define("sendJoinMessage", true);
+        SEND_JOIN_MESSAGE = COMMON_BUILDER.comment("Activate whether the message should be sent or not").define("sendJoinMessage", true);
 
-        START_ITEMS = COMMON_BUILDER.comment("Here are the items that every player gets when they join a world or a server for the first time").define("first_items", Lists.newArrayList("minecraft:dirt", "ftbquests:book", "intelligencetweaks:silver_coin"),
+        START_ITEMS = COMMON_BUILDER.comment("The items to be given to a player when they join a world or server for the first time.").define("first_items", Lists.newArrayList("minecraft:dirt", "ftbquests:book", "intelligencetweaks:silver_coin"),
                 it -> it instanceof String);
 
-        JOIN_MESSAGE = COMMON_BUILDER.comment("This is the message that every player gets when they join a world or a server.")
+        JOIN_MESSAGE = COMMON_BUILDER.comment("The message to be send to a player when they join a world or server.")
                 .define("joinMessage", "Thank you for playing Intelligence: New Horizon. If you want, you can join the Discord https://discord.srendi.de or leave a review on Curseforge.");
 
         COMMON_BUILDER.pop();
